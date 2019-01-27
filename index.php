a119aba8b798673e7a4ed6d5e9e75d848be71745
GUDRA <darlington@gudra-studio.com>
2019-01-27 11:48:09 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,73 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>minoHealth: Innovative Medical Health System</title>
+<link rel="stylesheet" type="text/css" href="CSS/style.css" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+<meta name="theme-color" content="#100E3E" />
+</head>
+
+<body>
+ <div id="wrap">
+  <div id="header">
+   <div id="logo-nav-wrap">
+     <a href=""><div id="logo"><div id="logo-img"></div></div></a>
+     <div id="nav-bar">
+      <ul>
+        <li id="nav-about"><a href="index.php#about">About</a></li>
+        <li id="nav-contacts"><a href="index.php#contacts">Contacts</a></li>
+        <li><a href="https://ai.minohealth.com" target="_blank">AI Labs</a></li>
+      </ul>
+     </div>
+   </div>
+   <p>Innovative Medical Health System seeking to Democratize Quality Healthcare with Artificial Intelligence
+    for Medical Predictions and Diagnoses, Cloud Medical Records System for Hospitals, Heath Ministries and Patients 
+    and Health Data Analysis.</p>
+  </div>
+  <div id="about">
+    <h1>About</h1>
+    <p>minoHealth is a multifaceted system developed with
+the objective of democratising Quality Healthcare with innovative and cutting edge
+technologies like Artificial Intelligence, Data Science, Cloud computing and Biotechnology in
+Africa. minoHealth and its AI Research lab, <a href="https://ai.minohealth.com" target="_blank">minoHealth AI Labs</a> researches and experiments
+with Artificial Intelligence and ways it can be applied to Healthcare to make it faster, better
+and yet, cheaper.</p><br><br>
+    <h3>minoHealth: The multifaceted system</h3>
+<p>minoHealth is a multifaceted system that combines Artificial Intelligence, Biotechnology(currently research), Big Data/Data Analysis and Cloud Computing in order to optimise processes involved in Healthcare and also make them affordable and scalable
+compared to the current practices especially in Africa. minoHealth uses Artificial
+Intelligence to make Health Predictions/Forecasts, Diagnoses and Prognoses and organises and stores Patient Health Records in the cloud with
+‘Hospital portal’ and ‘Central Portal’ for authorised healthcare personnels. All Data
+collected is intelligently organised with ‘Big Data’ approaches and technologies, they are
+continually analysed so important health information/stats on health center patients are
+visualised and made available to physicians.</p><br>
+
+<p>At the same time, with the permission of the various health centers, all the health data are
+also automatically saved and organised within the ‘Central Portal’ which allows for National
+Health Records Databases. This could also be extended even for the continent. Analysed
+and visualized health information/stats is a really good source of studying and noting early
+patterns of outbreaks nationally or continentally, so they can be managed beforeescalation.
+Collected data is used to further train and develop more Artificial Intelligence Healthcare models and systems.</p><br><br>
+  </div>
+  <div id="contacts">
+   <div id="contacts-img"><h1>Contacts</h1></div>
+   <div id="contacts-info">
+     <span><a href="mailto:contact@minohealth.com">contact@minohealth.com</a></span><br>
+     <span><a href="tel:+233504049188">+233 50 404 9188</a></span><br>
+     <span><a href="tel:+233504049188">+233 55 180 0233</a></span><br>
+     
+     <h4>Founder</h4>
+     <span>Darlington Ahiale Akogo</span><br>
+     <span><a href="mailto:darlington@gudra-studio.com">darlington@gudra-studio.com</a></span><br><br><br><br>
+     
+     <a href="https://m.facebook.com/minoHealth/" id="facebook-href" target="_blank"><img src="images/facebook-logo.png" id="facebook" alt="Facebook" /></a> &nbsp;
+     <a href="https://mobile.twitter.com/minoHealth" target="_blank"><img src="images/twitter-logo-on-black-background.png" id="twitter" alt="Twitter" /></a> &nbsp;
+     <a href="https://www.instagram.com/minohealth/" target="_blank"><img src="images/instagram-social-network-logo-of-photo-camera.png" id="instagram" alt="Instagram" /></a>
+   </div>
+  </div>
+  <div id="footer">
+  <p>minoHeath © 2018. Designed by <a href="https://gudra-studio.com" target="_blank">Gudra</a></p>
+  </div>
+ </div>
+</body>
+</html>
